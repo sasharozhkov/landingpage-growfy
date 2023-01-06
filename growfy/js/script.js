3bbfc62f960fca8da969f7922d38b27715c1356d
sasharozhkov <alexander_rozhkov@icloud.com>
2023-01-06 10:51:50 +0300

    feat: add functional burger

--- growfy/js/script.js
+++ growfy/js/script.js
@@ -1,1 +1,10 @@
 "user strict"
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+    const targetItem = e.target;
+    if (targetItem.closest('.icon-menu')) {
+        document.documentElement.classList.toggle('menu-open');
+    }
+}
