bf5c07d40bece2d0c8358f43dbab749a7c0020c1
sasharozhkov <alexander_rozhkov@icloud.com>
2023-01-11 14:45:45 +0300

    feat: add status bar and animation elemets js

--- growfy/js/script.js
+++ growfy/js/script.js
@@ -1,9 +1,67 @@
 "user strict"
 
+// ===================================================
+
+/* progress bar */
+window.onscroll = function () { myFunction() };
+
+const header = document.querySelector('wrapper');
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    var top = window.scrollY;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// ===================================================
+
+/* burger_icon */
 document.addEventListener('click', documentClick);
 function documentClick(e) {
     const targetItem = e.target;
     if (targetItem.closest('.icon-menu')) {
         document.documentElement.classList.toggle('menu-open');
     }
+}
+
+// ===================================================
+
+// animation
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 500)
+
 }
